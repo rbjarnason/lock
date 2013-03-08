f04cef36fbf0db38b661811b94e8620a929d36eb
Róbert Viðar Bjarnason <robert.bjarnason@gmail.com>
2013-03-08 12:35:41 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   match "lock/login", :to=>"lock#login", :as=>"lock_login", :via=>:get
-  match "lock/refused", :to=>"lock#login", :as=>"lock_login", :via=>:get  
+  match "lock/refused", :to=>"lock#login", :as=>"unlock_refused_url", :via=>:get  
   match "lock/unlock", :to=>"lock#unlock", :as=>"unlock", :via=>:post
 end
